da7da16bc4805fcfbb9942fd1ba7a574e5390991
Smthy <61051295+Smthy@users.noreply.github.com>
2020-04-02 18:25:04 +0100

    Ai In Game
    Basic at the moment

---
+++ The Darkness/Assets/Scripts/EnemyProjectile.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProjectile : MonoBehaviour
+{
+    public float speed;
+
+    private Transform player;
+    private Vector2 target;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        target = new Vector2(player.position.x, player.position.y);
+    }
+    
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+        if(transform.position.x == target.x && transform.position.y == target.y)
+        {
+            DestroyProjectile();
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void DestroyProjectile()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ The Darkness/Assets/Scripts/Shooting.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject magicPrefab;
+
+    public float magicForce = 20f;
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject magic = Instantiate(magicPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = magic.GetComponent<Rigidbody2D>();
+
+        rb.AddForce(firePoint.up * magicForce, ForceMode2D.Impulse);
+    }
+}
